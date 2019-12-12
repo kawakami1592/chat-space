17c63aab3bedd195ef62bdd5a8e6d94abfe69ca3
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2019-12-12 18:58:30 +0900

    時間の表示を日本時間に変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.time_zone = 'Tokyo'
     end
     config.i18n.default_locale = :ja
   end
